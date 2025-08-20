543aae83301dddbf04ef8246046b4f53e0062928
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-20 17:59:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,10 @@ def news_detail(request: Request, news_id: int):
             "year": datetime.now().year
         })
     return HTMLResponse(content="新闻不存在", status_code=404)
+
+import os
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render 会自动提供 PORT
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
