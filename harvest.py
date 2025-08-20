16b6615e7746713746dd9e5212a2872edd83f179
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-20 13:27:32 +0800

    Update harvest.py

--- harvest.py
+++ harvest.py
@@ -1,6 +1,7 @@
 import requests
-from bs4 import BeautifulSoup from database
-import random，insert_news, get_db
+from bs4 import BeautifulSoup 
+from database import insert_news, get_db
+import random
 
 def simple_rewrite(text):
     """
