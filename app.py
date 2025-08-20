82d66e23b099479ecb23e90ceefad83f12c90652
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-20 14:08:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from harvest import fetch_news, init_db
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-# 启动事件，初始化数据库
 @app.on_event("startup")
 def startup_event():
     init_db()
 
-@app.get("/")
-def home():
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
     news = fetch_news()
-    return {"news": news}
+    return templates.TemplateResponse("index.html", {"request": request, "news": news})
