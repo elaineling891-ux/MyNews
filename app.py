b3ed2c014365bdaf84b2d561b42d718511440f5a
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-21 08:23:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render 会自动提供 PORT
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
