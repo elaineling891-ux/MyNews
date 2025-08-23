c6c751552dd44d11b14b791efa25839bf027b17d
elaineling891-ux <elaine.ling891@gmail.com>
2025-08-23 14:12:06 +0800

    Update harvest.py

--- harvest.py
+++ harvest.py
@@ -144,7 +144,7 @@ def fetch_site_news(url, limit=20):
         elif "yahoo.com" in url:
             items = soup.select("h3 a span") or soup.select("a[aria-label]")
         elif "sinchew.com.my" in url:
-            items = soup.select("h5.entry-title a")
+            items = soup.select("div.post-title a")
         else:
             items = []
 
